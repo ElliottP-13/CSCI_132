d2e11013df4fc7679682aa8f301e486cba213029
Elliott Pryor <pryorelliott@gmail.com>
2018-10-11 12:44:43 -0600

    Lab 5 2.0

--- src/Labs/ReverseGeneric.java
+++ src/Labs/ReverseGeneric.java
@@ -1,21 +1,31 @@
 package src.Labs;
 
-public class ReverseGeneric<E> {
-    private E[] array;
-    public ReverseGeneric(E[] arr){
-        array = arr;
+import java.util.Arrays;
 
-        for (E x : arr){
-            System.out.println("Element: " + x.toString());
+public class ReverseGeneric {
+    public static void main(String[] args) {
+        String[] s = new String[]{"A", "B", "C", "D", "E"};
+        Integer[] i = new Integer[]{1, 2, 3, 4, 5, 6};
+        Song[] songs = new Song[5];
+        for(int x= 0; x < songs.length; x++){
+            songs[x] = new Song("Song " + x, "Artist " + x);
         }
+        reverseArray(s);
+        reverseArray(i);
+        reverseArray(songs);
+        System.out.println(Arrays.toString(s));
+        System.out.println(Arrays.toString(i));
+        System.out.println(Arrays.toString(songs));
+
     }
 
     public static <E> void reverseArray(E[] array){
         int low = 0, high = array.length - 1;
-        for (int i = 0; i < array.length; i++) {
+        while (low < high) {
             E temp = array[low];
             array[low++] = array[high];    // post-increment of low
             array[high--] = temp;        // post-decrement of high
         }
     }
+
 }
